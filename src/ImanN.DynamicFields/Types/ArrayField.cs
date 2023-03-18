cf5b52ae67ae6342b0ad3ab063aa60fe2e937ae4
imun <imun22@gmail.com>
2023-03-18 12:28:54 +0330

    add ArrayField and UnitTest project

--- src/ImanN.DynamicFields/Types/ArrayField.cs
+++ src/ImanN.DynamicFields/Types/ArrayField.cs
@@ -19,7 +19,37 @@ public class ArrayField<TValue> : DynamicField<Guid, ArrayFieldValue<TValue>>
 }
 
 
-public class ArrayFieldValue<TValue> : List<TValue>
+public class ArrayFieldValue<TValue>
 {
+    private Dictionary<int, TValue> _values = new();
+    private int _lastIndex = 0;
+
+    public ArrayFieldValue()
+    {
+    }
+
+    public ArrayFieldValue(IReadOnlyCollection<TValue> values)
+    {
+        if (values is null) throw new ArgumentNullException(nameof(values));
+        Add(values);
+    }
+
+    public void RemoveAll()
+    {
+        _values.Clear();
+        _lastIndex = 0;
+    }
     
+    public void Add(IReadOnlyCollection<TValue> values)
+    {
+        foreach (var v in values) Add(v);
+    }
+
+    public void Add(TValue value)
+    {
+        if (value is null) throw new ArgumentNullException(nameof(value));
+        
+        _values.Add(_lastIndex, value);
+        _lastIndex++;
+    }
 }
