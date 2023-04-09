6a355babddfd72b5da1d44019e33b45ec1dcfa75
m.gohari <0r0.developer@gmail.com>
2023-04-09 15:40:55 +0330

    date_field_can_not_be_defined_with_value_more_than_min_date_constraint unit test

--- test/DynamicFields.UnitTests/Date/date_tests.cs
+++ test/DynamicFields.UnitTests/Date/date_tests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ImanN.DynamicFields;
+using ImanN.DynamicFields.Exceptions;
 using Xunit;
 
 namespace DynamicFields.UnitTests.Date;
@@ -8,7 +10,6 @@ namespace DynamicFields.UnitTests.Date;
 public class date_tests
 {
     [Fact]
-
     public void date_field_can_be_defined_with_valid_properties()
     {
         var date = new DateTestField(new DynamicFieldId<Guid>(Guid.NewGuid()),
@@ -16,8 +17,37 @@ public class date_tests
 
         date.Set(2022, 10, 10);
         var expectedDate = new DateOnly(2022, 10, 10);
-        Assert.Equal(date.Value,expectedDate);
-
-
+        Assert.Equal(expectedDate,date.Value);
     }
+
+
+    [Fact]
+    public void date_field_can_not_be_defined_with_value_more_than_max_date_constraint()
+    {
+        var date = new DateField(new DynamicFieldId<Guid>(Guid.NewGuid()),"Date","Date",false,
+        new List<Constraint<DateOnly>>()
+        {
+            new MaxDateConstraint(new DateOnly(2023,03,03))
+        });
+        date.Set(2024,1,1);
+        var  a = date.Constraints.First();
+        Assert.False(a.IsSatisfiedBy(date.Value));
+        Assert.Throws<MaxDateException>(()=>a.Validate(date.Value));
+    }
+    
+    [Fact]
+    public void date_field_can_not_be_defined_with_value_more_than_min_date_constraint()
+    {
+        var date = new DateField(new DynamicFieldId<Guid>(Guid.NewGuid()),"Date","Date",false,
+            new List<Constraint<DateOnly>>()
+            {
+                new MinDateConstraint(new DateOnly(2023,03,03))
+            });
+        date.Set(2022,1,1);
+        var a = date.Constraints.First();
+        Assert.False(date.Constraints.First().IsSatisfiedBy(date.Value));
+        Assert.Throws<MinDateException>(()=>a.Validate(date.Value));
+    }
+    
+    
 }
