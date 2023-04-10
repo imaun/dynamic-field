274ba82911d8ab41965936211471ae23ef1ee3b8
m.gohari <0r0.developer@gmail.com>
2023-04-10 15:34:48 +0330

    define_date_range_with_max_date_constraint unit test implementation

--- src/ImanN.DynamicFields/Types/DateRange/Constraints/MaxDateRangeConstraint.cs
+++ src/ImanN.DynamicFields/Types/DateRange/Constraints/MaxDateRangeConstraint.cs
@@ -2,13 +2,18 @@ using ImanN.DynamicFields.Exceptions;
 
 namespace ImanN.DynamicFields.Types.DateRange.Constraints;
 
-public class MaxDateConstraint : Constraint<DateRangeValue>
+public class MaxDateRangeConstraint : Constraint<DateRangeValue>
 {
     public override string Name => "Max.DateRange";
 
+    public MaxDateRangeConstraint(DateRangeValue value)
+    {
+        Value = value;
+    }
+
     public override bool IsSatisfiedBy(DateRangeValue value)
     {
-        return Value.EndDate < value.EndDate;
+        return Value.EndDate > value.EndDate;
     }
 
     public override void Validate(DateRangeValue value)
--- test/DynamicFields.UnitTests/DateRange/date_range_tests.cs
+++ test/DynamicFields.UnitTests/DateRange/date_range_tests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using ImanN.DynamicFields;
+using ImanN.DynamicFields.Exceptions;
 using ImanN.DynamicFields.Types.DateRange;
+using ImanN.DynamicFields.Types.DateRange.Constraints;
 using Xunit;
 
 namespace DynamicFields.UnitTests.DateRange;
@@ -15,7 +17,19 @@ public class date_range_tests
             , false, new List<Constraint<DateRangeValue>>());
         dateRange.SetValue(new DateRangeValue(DateTime.Today, DateTime.Today.AddDays(2)));
         var expectedDate = new DateRangeValue(DateTime.Today, DateTime.Today.AddDays(2));
-        Assert.Equal(expectedDate.StartDate,dateRange.Value.StartDate);
-        Assert.Equal(expectedDate.EndDate,dateRange.Value.EndDate);
+        Assert.Equal(expectedDate.StartDate, dateRange.Value.StartDate);
+        Assert.Equal(expectedDate.EndDate, dateRange.Value.EndDate);
+    }
+
+    [Fact]
+    public void define_date_range_with_max_date_constraint()
+    {
+        var dateRange = new DateRangeField(new DynamicFieldId<Guid>(Guid.NewGuid()), "DateRange", "DateRange", false
+            , new List<Constraint<DateRangeValue>>()
+            {
+                new MaxDateRangeConstraint(new DateRangeValue(DateTime.Today, DateTime.Today.AddDays(3)))
+            });
+        var dateRangeError = ()=>dateRange.SetValue(new DateRangeValue(DateTime.Today,DateTime.Today.AddDays(4)));
+        Assert.Throws<DateRangeMaxException>(dateRangeError);
     }
 }
