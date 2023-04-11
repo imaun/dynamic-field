92960b63fbe128332943c300e12c4fb60f4580e6
m.gohari <0r0.developer@gmail.com>
2023-04-11 12:05:13 +0330

    defined_date_time_with_min_date_time_constraint unit test

--- test/DynamicFields.UnitTests/DateTime/date_time_tests.cs
+++ test/DynamicFields.UnitTests/DateTime/date_time_tests.cs
@@ -30,4 +30,16 @@ public class date_time_tests
         var dateTimeException = () => dateTimeField.SetValue(new System.DateTime(2025, 10, 10));
         Assert.Throws<MaxDateTimeException>(dateTimeException);
     }
+
+    [Fact]
+    public void defined_date_time_with_min_date_time_constraint()
+    {
+        var dateTimeField = new DateTimeField(new DynamicFieldId<Guid>(Guid.NewGuid()), "DateTime",
+            "DateTime", false, new List<Constraint<System.DateTime>>
+            {
+                new MinDateTimeConstraint(new System.DateTime(2024, 01, 01))
+            });
+        var dateTimeException = () => dateTimeField.SetValue(new System.DateTime(2023, 10, 10));
+        Assert.Throws<MinDateTimeException>(dateTimeException);
+    }
 }
